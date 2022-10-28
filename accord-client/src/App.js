220fd2697b8d56a2fa35eecdad12d4a17614a0e9
Paulo Barata <pbara7a@gmail.com>
2022-10-28 23:23:11 +0100

    Servers sidebar

--- accord-client/src/App.js
+++ accord-client/src/App.js
@@ -1,5 +1,14 @@
+import { Grid } from "@mui/material";
+import ServersBar from "./components/ServersBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Grid container>
+      <Grid item columns={1}>
+        <ServersBar />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ accord-client/src/components/ServersBar.js
@@ -0,0 +1,40 @@
+import { Grid, Avatar } from "@mui/material";
+
+// Placeholders
+const servers = [
+  {
+    img: "https://imgs.search.brave.com/prjQBDSyJtEpt6TzO4mzQasHpl5wY2YsJ4SI2uF3LH0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vc3VwZXJi/b3dsLWFkcy5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MDEvcHJpbmdsZXNf/bG9nby5qcGVnP2Zp/dD0yNzAwJTJDMjA4/Ng",
+    namespaceTitle: "Pringles1",
+  },
+  {
+    img: "https://imgs.search.brave.com/prjQBDSyJtEpt6TzO4mzQasHpl5wY2YsJ4SI2uF3LH0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vc3VwZXJi/b3dsLWFkcy5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MDEvcHJpbmdsZXNf/bG9nby5qcGVnP2Zp/dD0yNzAwJTJDMjA4/Ng",
+    namespaceTitle: "Pringles2",
+  },
+  {
+    img: "https://imgs.search.brave.com/prjQBDSyJtEpt6TzO4mzQasHpl5wY2YsJ4SI2uF3LH0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vc3VwZXJi/b3dsLWFkcy5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MDEvcHJpbmdsZXNf/bG9nby5qcGVnP2Zp/dD0yNzAwJTJDMjA4/Ng",
+    namespaceTitle: "Pringles3",
+  },
+];
+
+function ServersBar() {
+  return (
+    <Grid
+      container
+      direction="column"
+      padding={2}
+      gap={1}
+      sx={{ background: "#292b2c" }}
+    >
+      {servers.map((server, i) => (
+        <Avatar
+          key={i}
+          alt={server.namespaceTitle}
+          src={server.img}
+          sx={{ width: 48, height: 48 }}
+        />
+      ))}
+    </Grid>
+  );
+}
+
+export default ServersBar;
