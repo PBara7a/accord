8561a1bf0b6444d5adde8060420c36999cdc95c0
Paulo Barata <pbara7a@gmail.com>
2022-10-29 15:10:14 +0100

    Room class

---
+++ accord-server/classes/Room.js
@@ -0,0 +1,16 @@
+class Room {
+  constructor(id, roomTitle, namespace) {
+    this.id = id;
+    this.roomTitle = roomTitle;
+    this.namespace = namespace;
+    this.history = [];
+  }
+
+  addMessage(message) {
+    this.history.push(message);
+  }
+
+  clearHistory() {
+    this.history = [];
+  }
+}
